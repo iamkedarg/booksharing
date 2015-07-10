00a1f75826e6a3745dc8b69d089961a7ea783051
KuldeepJadhav <kuldeep.jadhav@live.com>
2015-07-10 18:38:59 +0530

    Refactored some code

--- server/detailsupload/detailsController.js
+++ server/detailsupload/detailsController.js
@@ -1,46 +1,30 @@
 var multiparty = require('multiparty');
 var fs = require('fs');
+var DetailBuilder = require('./detailBuilder');
+var deepcopy = require('deepcopy');
+var detailsService = require('./detailsService');
 module.exports = function(app) {
 	app.post('/upload', function(req, res, next) {
 		var form = new multiparty.Form();
-
-		// {
-		// 	'autoFiles': true,
-		// 	'uploadDir': '/Users/jadhavk/uploads'
-		// }
-
-
-		// form.parse(req, function(err, fields, files) {
-		// 	console.log('fields are ' + JSON.parse(fields));
-		// 	// var data = JSON.parse(fields.post);
-		// 	// var name = data.name;
-		// 	// if(files.file) {
-		// 	// 	for(var i=0 ;i<files.file.length; i++) {
-		// 	// 		var actualFile = files.file[i];
-		// 	// 		var origionalFileName = actualFile.originalFilename;
-		// 	// 		var path = actualFile.path;
-		// 	// 	}
-		// 	// }
-		// });
-
 		var count = 0;
+		var detailBuilder = new DetailBuilder();
 		form.on('error', function(err) {
 			console.log('Error parsing form: ' + err.stack);
 		});
 
-
 		form.on('part', function(part) {
 			if (!part.filename) {
-				console.log('got field named ' + part.name);
+				if(part.id) {
+					detailBuilder.id(deepcopy(part.id));
+				}else if(part.tags) {
+					detailBuilder.tags(deepcopy(part.tags));
+				}
 				part.resume();
 			}
-
+			
 			if (part.filename) {
 				count++;
-				var writable = fs.createWriteStream('/Users/jadhavk/uploads/file.doc');
-				console.log('got file named ' + part.name);
-				console.log('file path ' + typeof part);
-				part.pipe(writable);
+				detailBuilder.fileReadableStream(deepcopy(part));
 				part.resume();
 			}
 
@@ -51,6 +35,8 @@ module.exports = function(app) {
 
 		// Close emitted after form parsed 
 		form.on('close', function() {
+			var details = detailBuilder.build();
+			detailsService.uploadDetails(details);
 			console.log('Upload completed!');
 			res.end('Received ' + count + ' files');
 		});
