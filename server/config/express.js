3c0921411b6ec543ca9a9b605e0e03045e1dbbcd
KuldeepJadhav <kuldeep.jadhav@live.com>
2015-07-10 14:50:45 +0530

    added file upload

--- server/config/express.js
+++ server/config/express.js
@@ -13,13 +13,13 @@ var methodOverride = require('method-override');
 var cookieParser = require('cookie-parser');
 var errorHandler = require('errorhandler');
 var path = require('path');
-var config = require('./environment');
+//var config = require('./environment');
 var passport = require('passport');
 
 module.exports = function(app) {
   var env = app.get('env');
 
-  app.set('views', config.root + '/server/views');
+ // app.set('views', config.root + '/server/views');
   app.engine('html', require('ejs').renderFile);
   app.set('view engine', 'html');
   app.use(compression());
@@ -28,18 +28,18 @@ module.exports = function(app) {
   app.use(methodOverride());
   app.use(cookieParser());
   if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, 'public')));
-    app.set('appPath', config.root + '/public');
-    app.use(morgan('dev'));
+    // app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+    // app.use(express.static(path.join(config.root, 'public')));
+    // app.set('appPath', config.root + '/public');
+    // app.use(morgan('dev'));
   }
 
   if ('development' === env || 'test' === env) {
-    app.use(require('connect-livereload')());
-    app.use(express.static(path.join(config.root, '.tmp')));
-    app.use(express.static(path.join(config.root, 'client')));
-    app.set('appPath', 'client');
-    app.use(morgan('dev'));
-    app.use(errorHandler()); // Error handler - has to be last
+    // app.use(require('connect-livereload')());
+    // app.use(express.static(path.join(config.root, '.tmp')));
+    // app.use(express.static(path.join(config.root, 'client')));
+    // app.set('appPath', 'client');
+    // app.use(morgan('dev'));
+    // app.use(errorHandler()); // Error handler - has to be last
   }
 };
